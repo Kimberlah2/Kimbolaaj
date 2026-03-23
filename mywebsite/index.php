0e047698bb8c547cf7aeaf933b5e1205db19e439
Kimberlah2 <noverokimberly4@gmail.com>
2026-03-23 18:49:31 +0800

    Add files via upload

---
+++ mywebsite/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+   <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <h1>Kimberly Reyn B. Novero</h1>
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+  </div>
+   
+    <div class="container" style="background-color:#f1f1f1">
+  </div>
+
+</body>
+</html>
